8e6921b7be2e576720fc7fb38418b8dc5a1e8317
eugenedefox <64322069+eugenedefox@users.noreply.github.com>
2024-03-06 20:53:18 +0300

    Add files via upload

---
+++ linediredeboost/line_2d .py
@@ -0,0 +1,68 @@
+<<<<<<< HEAD
+import time
+from ahk import AHK
+ahk = AHK()
+import pyautogui
+import asyncio
+
+import pydirectinput
+import keyboard
+
+def active_window_2d():
+    pyautogui.moveTo(804,467)
+    pydirectinput.doubleClick()
+    time.sleep(0.3)
+
+
+active_window_2d()
+def map_navigation_2d():
+    pydirectinput.moveTo(1183,421)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+map_navigation_2d()
+
+
+
+pyautogui.sleep(40)
+def map_navigation_2d_2move():
+    pyautogui.sleep(0.3)
+    pydirectinput.moveTo(1179,448)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+=======
+import time
+from ahk import AHK
+ahk = AHK()
+import pyautogui
+import asyncio
+
+import pydirectinput
+import keyboard
+
+def active_window_2d():
+    pyautogui.moveTo(804,467)
+    pydirectinput.doubleClick()
+    time.sleep(0.3)
+
+
+active_window_2d()
+def map_navigation_2d():
+    pydirectinput.moveTo(1183,421)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+map_navigation_2d()
+
+
+
+pyautogui.sleep(40)
+def map_navigation_2d_2move():
+    pyautogui.sleep(0.3)
+    pydirectinput.moveTo(1179,448)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+>>>>>>> origin/main
+map_navigation_2d_2move()
---
+++ linediredeboost/line_4d .py
@@ -0,0 +1,55 @@
+<<<<<<< HEAD
+import time
+from ahk import AHK
+ahk = AHK()
+import pyautogui
+import asyncio
+
+import pydirectinput
+import keyboard
+
+def active_window_4d():
+    pyautogui.moveTo(163,977)
+    pydirectinput.doubleClick()
+    time.sleep(0.3)
+
+
+active_window_4d()
+def map_navigation_4d():
+    pydirectinput.moveTo(573,976)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+map_navigation_4d()
+
+pyautogui.sleep(40)
+
+
+=======
+import time
+from ahk import AHK
+ahk = AHK()
+import pyautogui
+import asyncio
+
+import pydirectinput
+import keyboard
+
+def active_window_4d():
+    pyautogui.moveTo(163,977)
+    pydirectinput.doubleClick()
+    time.sleep(0.3)
+
+
+active_window_4d()
+def map_navigation_4d():
+    pydirectinput.moveTo(573,976)
+    pyautogui.sleep(0.3)
+    ahk.right_click()
+
+map_navigation_4d()
+
+pyautogui.sleep(40)
+
+
+>>>>>>> origin/main
